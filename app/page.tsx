83f14211d2a55506d3ccdaae24d8167afc7bc0aa
Aleksandar Pletl <pletl.aleksandar@gmail.com>
2024-05-28 16:57:22 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,9 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Question from "../components/question";
-import {questions} from '../data/mocks';
-import {useState} from 'react';
+import { questions } from '../data/mocks';
+import { useState } from 'react';
+import { motion } from 'framer-motion';
 
 interface QuestionPair {
     id: number;
@@ -17,14 +18,14 @@ export default function Home() {
 
     const Initialize = () => {
         questions.forEach((q) => {
-            const p: QuestionPair = { id: q.id, isAnswerCorrect: false};
+            const p: QuestionPair = { id: q.id, isAnswerCorrect: false };
             questionPairs.push(p);
         });
     }
     Initialize();
 
     const answerChanged = (id: number, answeredCorrectly: boolean) => {
-        questionPairs.forEach((qp)=> {
+        questionPairs.forEach((qp) => {
             if (qp.id === id) {
                 qp.isAnswerCorrect = answeredCorrectly;
             }
@@ -38,12 +39,12 @@ export default function Home() {
     const updateAnswerSummary = () => {
         let allAnswersAreCorrect = true;
 
-        questionPairs.forEach((qp)=> {
+        questionPairs.forEach((qp) => {
             if (!qp.isAnswerCorrect) {
                 allAnswersAreCorrect = false;
             }
         });
-        
+
         setAllAnswersCorrect(allAnswersAreCorrect);
     }
 
@@ -60,8 +61,8 @@ export default function Home() {
             </div>
 
             <div className={styles.center}>
-                {questions.map((q)=> (
-                    <Question key={q.id} id={q.id} questionText={q.questionText} answerExpected={q.answerExpected} cipher={q.cipher} onAnswered={answerChanged}/>
+                {questions.map((q) => (
+                    <Question key={q.id} id={q.id} questionText={q.questionText} answerExpected={q.answerExpected} cipher={q.cipher} onAnswered={answerChanged} />
                 ))}
             </div>
 
