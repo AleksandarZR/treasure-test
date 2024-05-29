e61999c9f48b9547f3141a7f32912aca8a2907e8
Aleksandar Pletl <pletl.aleksandar@gmail.com>
2024-05-29 15:49:45 +0200

    More questions

--- data/mocks.ts
+++ data/mocks.ts
@@ -1,5 +1,5 @@
 export const questions = [
-    /* {
+    {
         id: 1,
         questionText: "Koje godine se odigrala Kosovska bitka?",
         answerExpected: ["1389"],
@@ -16,7 +16,7 @@ export const questions = [
         questionText: "Koliko krakova ima oktopod?",
         answerExpected: ["8"],
         cipher: "✓",
-    }, */
+    },
     {
         id: 4,
         questionText: "Kako se zove prestonica Italije?",
@@ -27,6 +27,36 @@ export const questions = [
         id: 5,
         questionText: "Who is the most powerful man in the universe?",
         answerExpected: ["He-Man", "He-man", "Heman", "HeMan", "Himen"],
+        cipher: "✓",
+    },
+    {
+        id: 6,
+        questionText: "Koliko stranica ima heksagon?",
+        answerExpected: ["6"],
+        cipher: "✓",
+    },
+    {
+        id: 7,
+        questionText: "Kojim jezikom se govori u Brazilu?",
+        answerExpected: ["portugalski", "Portugalski", "portugalskim", "Portugalskim"],
+        cipher: "✓",
+    },
+    {
+        id: 8,
+        questionText: "Koji je glavni grad Kanade?",
+        answerExpected: ["Otava"],
+        cipher: "✓",
+    },
+    {
+        id: 9,
+        questionText: "Atomsko jezgro sadrži protone i:",
+        answerExpected: ["neutrone", "Neutrone"],
+        cipher: "✓",
+    },
+    {
+        id: 10,
+        questionText: "Ako je današnje sutra sreda, šta je prekjučerašnje juče?",
+        answerExpected: ["subota", "Subota"],
         cipher: "✓",
     },
 ];
